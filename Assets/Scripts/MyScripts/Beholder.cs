db949cbf139b2a4cc05dc95e2f4f6d44c0ac1f54
Kovalev1993 <Air1618@gmail.com>
2024-02-19 22:56:12 +0300

    Beholder death

--- Assets/Scripts/MyScripts/Beholder.cs
+++ Assets/Scripts/MyScripts/Beholder.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,15 @@ public class Beholder : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private string _animationSpeedVariable;
+    [Header("Eyes defence")]
     [SerializeField] private List<BeholderEye> _eyes = new();
     [SerializeField] private float _attackingTime;
     [SerializeField] private Collider _triggerCollider;
+    [Header("Destroying")]
+    [SerializeField] private float _dethThresold;
+    [SerializeField] private float _fallDuration;
+    [SerializeField] private float _fallAltitudeY;
+    [SerializeField] private Ease _fallEasing;
 
     public void StartAnimations() => _animator.SetFloat(_animationSpeedVariable, 1);
 
@@ -24,6 +31,17 @@ public class Beholder : MonoBehaviour
 
         if(areAllEyesDamaged)
             _triggerCollider.enabled = false;
+    }
+
+    public void OnDeath(Vector3 destructionPoint, float relativeVelocity, float destructionPercentage)
+    {
+        var isAlive = _animator.enabled;
+        if(isAlive && _dethThresold <= destructionPercentage) {
+            _animator.enabled = false;
+            transform.DOMoveY(_fallAltitudeY, _fallDuration)
+                .SetEase(_fallEasing)
+                .Play();
+        }
     }
 
     private void OnTriggerEnter(Collider collider)
